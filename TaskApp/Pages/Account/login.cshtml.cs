f50716d18e88c931a728668c9d544ef4ddcf7286
Bimsara Vibuthi <bimsaravibuthi1541@gmail.com>
2022-01-11 15:27:43 +0530

    PUSH 1

---
+++ TaskApp/Pages/Account/login.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaskApp.Pages.Account
+{
+    public class loginModel : PageModel
+    {
+        [BindProperty]
+        public Credantial Credantial { get; set; }
+        public void OnGet()
+        {
+            this.Credantial = new Credantial { UserName = "Admin" };
+        }
+
+        public void OnPost()
+        {
+
+        }
+    }
+
+    public class Credantial
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}

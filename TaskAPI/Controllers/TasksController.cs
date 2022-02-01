2e00bbe8eea194b5c959e950d58c89c55c2287b4
Bimsara Vibuthi <bimsaravibuthi1541@gmail.com>
2022-02-01 15:12:14 +0530

    PUSH 01 15:12

---
+++ TaskAPI/Controllers/TasksController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]   //Select
+        public IActionResult Tasks()
+        {
+            var Tasks = new string[] { "Task 1", "Task 2", "Task 3" };
+            return Ok(Tasks);
+        }
+
+        [HttpPost]  //Insert
+        public IActionResult NewTask()
+        {
+            return Ok();
+        }
+
+        [HttpPut]   //Update
+        public IActionResult UpdateTask()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]    //Delete
+        public IActionResult DeleteTask()
+        {
+            return Ok();
+        }
+    }
+}

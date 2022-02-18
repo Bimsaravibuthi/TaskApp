94a3adeb74b4630d9980f20a7d6fe2b3862388ac
Bimsara Vibuthi <bimsaravibuthi1541@gmail.com>
2022-02-18 16:13:24 +0530

    PUSH 4:13 02/18

---
+++ TaskAPIDataAccess/Migrations/20220218103629_SeedData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaskAPIDataAccess.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Todos",
+                columns: new[] { "Id", "Created", "Description", "Due", "Status", "Title" },
+                values: new object[] { 1, new DateTime(2022, 2, 18, 16, 6, 28, 569, DateTimeKind.Local).AddTicks(2293), "Get some text books for school", new DateTime(2022, 2, 23, 16, 6, 28, 570, DateTimeKind.Local).AddTicks(1922), 0, "Get books for school - DB" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Todos",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
---
+++ TaskAPIDataAccess/TodoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskAPI.Models;
+
+namespace TaskAPIDataAccess
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=localhost\\SQLEXPRESS;Database=MyToDoDb;Trusted_Connection=True;MultipleActiveResultSets=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(new Todo 
+            {
+                Id = 1,
+                Title = "Get books for school - DB",
+                Description = "Get some text books for school",
+                Created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+            });
+        }
+    }
+}
